0d2bfd1867b77682f13e987501de8bf2b3dc9aba
audur <durazaurel@gmail.com>
2013-10-25 17:18:06 +0200

    commentaires

--- src/test/UserStoryTest.java
+++ src/test/UserStoryTest.java
@@ -9,27 +9,31 @@ import org.junit.Test;
 public class UserStoryTest {
 
 	@Test
-	public void testCreerUserStoryPlanifiee() throws Exception {
+	public void testUserStoryPlanifiee() throws Exception {
 		UserStory us = new UserStory("refonte du tunnel d'achat", 40);
 		assertTrue(us.getEtatUserStory() == EtatUserStory.PLANIFIEE);
 	}
 
 	@Test
-	public void testCreerUserStoryNonPlanifiee() throws Exception {
+	public void testUserStoryNonPlanifiee() throws Exception {
 		UserStory us = new UserStory("refonte du tunnel d'achat", 0);
 		assertTrue(us.getEtatUserStory() == EtatUserStory.NOUVELLE);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testNomNull() throws Exception {
+	public void testUserStoryNull() throws Exception {
 		new UserStory(null, 40);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testNomVide() throws Exception {
+	public void testUserStoryVide() throws Exception {
 		new UserStory(null, 40);
 	}
 
+	/**
+	 * s'assure que le RAF initial == charge
+	 * @throws Exception
+	 */
 	@Test
 	public void testRAFCharge() throws Exception {
 		UserStory us = new UserStory("Refonte de l'interface pour tablettes",
@@ -37,17 +41,24 @@ public class UserStoryTest {
 		assertTrue(us.getCharge() == us.getResteAFaire());
 	}
 
+	/**
+	 * test de la mise à jour du RAF pour une user story planifié.
+	 * @throws Exception
+	 */
 	@Test
-	public void testChangeRAFUsPlanifiee() throws Exception {
+	public void testSetRAFUserStoryPlanifiee() throws Exception {
 		UserStory us = new UserStory("Refonte de l'interface pour tablettes",
 				40);
 		assertTrue(us.getEtatUserStory() == EtatUserStory.PLANIFIEE);
 		us.setResteAFaire(0);
 		assertTrue(us.getEtatUserStory() == EtatUserStory.FERMEE);
 	}
-
+	/**
+	 * test de la mise à jour du RAF pour une user story nouvelle.
+	 * @throws Exception
+	 */
 	@Test(expected = IllegalArgumentException.class)
-	public void testChangeRAFUsNouvelle() throws Exception {
+	public void testSetRAFUserStoryNouvelle() throws Exception {
 		UserStory us = new UserStory("Refonte de l'interface pour tablettes", 0);
 		assertTrue(us.getEtatUserStory() == EtatUserStory.NOUVELLE);
 		us.setResteAFaire(10);
